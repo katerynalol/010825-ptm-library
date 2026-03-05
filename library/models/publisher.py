2bf69bc16513c16dd1309ff98f9a8de4c364974d
ThomasJager99 <artemzubchenko1@gmail.com>
2026-03-05 08:41:13 +0100

    problem with merge in db

--- library/models/publisher.py
+++ library/models/publisher.py
@@ -1,18 +1,16 @@
-from datetime import datetime
 
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 class Publisher(models.Model):
     name: str = models.CharField(
         max_length=100
     )
-    adress = str = models.CharField(
+    address: str = models.CharField(
         max_length=255,
         null=True,
         blank=True,
     )
-    city = str = models.CharField(
+    city: str = models.CharField(
         max_length=100,
         null=True,
         blank=True,
